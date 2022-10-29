b51460c2c4b441bc43305c8e248d049f017a7d83
Amogh <amoghjahagirdargsf@gmail.com>
2022-10-30 01:52:45 +0400

    game complete

--- src/Game.sol
+++ src/Game.sol
@@ -3,18 +3,143 @@ pragma solidity ^0.8.13;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 struct Coordinates {
-    int256 x; 
-    int256 y; 
+    uint256 x; 
+    uint256 y; 
 }
 
 contract Game is Ownable {
-    address owner;
-    uint256 roundNumber;
-    uint256 gameNumber;
-    Coordinates[] mines;
     
-    constructor(address _owner){
-        owner = _owner;
+     enum CoordinateStatus {
+        Zero,
+        One,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Mine,
+        Untouched
+     }
+
+    uint256 immutable public BOARDLENGTH = 9;
+    event GameEnded(bool playerWins, uint256 roundNumber, uint256 gameNumber);
+
+     uint256 public roundNumber;
+     uint256 public gameNumber;
+     uint256 private nonMineCount;
+
+    CoordinateStatus[BOARDLENGTH][BOARDLENGTH] private playerBoard;
+    CoordinateStatus[BOARDLENGTH][BOARDLENGTH] private realBoard;
+
+    Coordinates[] private mines;
+
+    constructor(Coordinates[] memory _mines){
+        mines = _mines;
+        nonMineCount = BOARDLENGTH*BOARDLENGTH - mines.length;
+        initializeGame();
+    }
+
+    function initializeGame() internal {
+        for(uint256 i = 0; i < mines.length;i++){
+            realBoard[mines[i].x][mines[i].y] = CoordinateStatus.Mine; 
+        }
+        intializeBoards();
+    }
+
+    function intializeBoards() internal {
+        for(uint256 i = 0; i < BOARDLENGTH; i++){
+            for(uint256 j = 0; j < BOARDLENGTH; j++){
+                playerBoard[i][j] = CoordinateStatus.Untouched;
+
+                if(realBoard[i][j] == CoordinateStatus.Mine){
+                    continue;
+                }
+
+                uint256 counter = 0;
+                counter =  isMine(i-1,j-1) ? counter + 1 : counter; 
+                counter =  isMine(i-1,j) ? counter + 1 : counter; 
+                counter =  isMine(i-1,j+1) ? counter + 1 : counter; 
+                counter =  isMine(i,j-1) ? counter + 1 : counter; 
+                counter =  isMine(i,j+1) ? counter + 1 : counter; 
+                counter =  isMine(i+1,j-1) ? counter + 1 : counter; 
+                counter =  isMine(i+1,j) ? counter + 1 : counter; 
+                counter =  isMine(i+1,j+1) ? counter + 1 : counter; 
+                realBoard[i][j] = CoordinateStatus(counter);
+            }
+        }
+    }
+
+    // TODO:
+    // function incrementIfMine
+    function isValid(uint256 x, uint256 y) internal view returns (bool) {
+        if( 0 <= x && x <= BOARDLENGTH && 0 <= y && y <= BOARDLENGTH){
+            return true;
+        }
+        return false;
+    }
+
+    function isMine(uint256 x, uint256 y) internal view returns (bool) {
+        if( isValid(x,y) && realBoard[x][y] == CoordinateStatus.Mine){
+            return true;
+        }
+        return false;
+    }
+
+    function processMove(uint256 x, uint256 y) public {
+        CoordinateStatus current = realBoard[x][y];
+
+        if(current != CoordinateStatus.Zero){
+            
+            playerBoard[x][y] = current;
+            nonMineCount--;
+            
+            if(hasPlayerWon()){
+                emit GameEnded(true, roundNumber, gameNumber);
+            }
+            return;
+        }
+
+        if(current == CoordinateStatus.Mine){
+            for(uint256 i = 0; i < mines.length; i++){
+                playerBoard[mines[i].x][mines[i].y] = CoordinateStatus.Mine;
+            }
+            emit GameEnded(false, roundNumber, gameNumber);
+            return;
+        } else {
+            nonMineCount--;
+
+            if(!isMine(x-1,y-1)){
+                processMove(x-1,y-1);
+            }
+            if(!isMine(x-1,y)){
+                processMove(x-1,y);
+            }
+            if(!isMine(x-1,y+1)){
+                processMove(x-1,y+1);
+            }
+            if(!isMine(x,y-1)){
+                processMove(x,y-1);
+            }
+            if(!isMine(x,y+1)){
+                processMove(x,y+1);
+            }
+            if(!isMine(x+1,y-1)){
+                processMove(x+1,y-1);
+            }
+            if(!isMine(x+1,y)){
+                processMove(x+1,y);
+            }
+            if(!isMine(x+1,y+1)){
+                processMove(x+1,y+1);
+            }
+        }
+        return;
+    }
+
+    function hasPlayerWon() public returns (bool) {
+        return nonMineCount == 0;
     }
 
 }
--- src/GameFactory.sol
+++ src/GameFactory.sol
@@ -1,22 +1,101 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
-import "./Game.sol"
+import "./Game.sol";
+import '@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol';
+import '@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol';
+import '@chainlink/contracts/src/v0.8/ConfirmedOwner.sol';
 
-contract GameFactory {
+
+contract GameFactory is VRFConsumerBaseV2, ConfirmedOwner {
     uint256 public currentRound;
     uint256 public timeTillNextRound;
     uint256 public numberOfGamesInCurrentRound;
 
+    /*
+    CHAINLINK VARS
+    */
+    event RequestSent(uint256 requestId, uint32 numWords);
+    event RequestFulfilled(uint256 requestId, uint256[] randomWords);
+
+    struct RequestStatus {
+        bool fulfilled; // whether the request has been successfully fulfilled
+        bool exists; // whether a requestId exists
+        uint256[] randomWords;
+    }
+    mapping(uint256 => RequestStatus) public s_requests; /* requestId --> requestStatus */
+    VRFCoordinatorV2Interface COORDINATOR;
+
+    // Your subscription ID.
+    uint64 s_subscriptionId;
+    bytes32 keyHash = 0x4b09e658ed251bcafeebbc69400383d49f344ace09b9576fe248bb02c003fe9f;
+    uint32 callbackGasLimit = 100000;
+    uint16 requestConfirmations = 3;
+    uint32 numWords = 80;
+
+    // past requests Id.
+    uint256[] public requestIds;
+    uint256 public lastRequestId;
+
+    constructor(uint256 subId) 
+        VRFConsumerBaseV2(0x7a1BaC17Ccc5b313516C5E16fb24f7659aA5ebed)
+        ConfirmedOwner(msg.sender) 
+    {
+        COORDINATOR = VRFCoordinatorV2Interface(0x7a1BaC17Ccc5b313516C5E16fb24f7659aA5ebed);
+        s_subscriptionId = subId;
+    }
+
+
     function createGame() public {
-        Game game = new Game(msg.sender);
+        // approval - contribute to pot
+        
+        // make request to chainlink
+
+        // create new game with obtained random number tokens
+
+        Game game = new Game(msg.sender, mines);
     }
 
-
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+    function generateCoordinates(uint256[] memory randomWords, uint256 numCoords) internal returns (Coordinates[] memory) {
+        require(len(randomWords) >= numCoords*2,"GameFactory : Too few random numbers generated");
+        Coordinates[numCoords] coords;
+        for(uint256 i = 0; i < numCoords; i++){
+            coords[i].x = randomWords[i];
+            coords[i].y = randomWords[i+1];
+        }
+        return coords;
     }
 
-    function increment() public {
-        number++;
+    /*
+    CHAINLINK FUNCS
+    */
+        // Assumes the subscription is funded sufficiently.
+    function requestRandomWords() external onlyOwner returns (uint256 requestId) {
+        // Will revert if subscription is not set and funded.
+        requestId = COORDINATOR.requestRandomWords(
+            keyHash,
+            s_subscriptionId,
+            requestConfirmations,
+            callbackGasLimit,
+            numWords
+        );
+        s_requests[requestId] = RequestStatus({randomWords: new uint256[](0), exists: true, fulfilled: false});
+        requestIds.push(requestId);
+        lastRequestId = requestId;
+        emit RequestSent(requestId, numWords);
+        return requestId;
     }
+
+    function fulfillRandomWords(uint256 _requestId, uint256[] memory _randomWords) internal override {
+        require(s_requests[_requestId].exists, 'request not found');
+        s_requests[_requestId].fulfilled = true;
+        s_requests[_requestId].randomWords = _randomWords;
+        emit RequestFulfilled(_requestId, _randomWords);
+    }
+
+    function getRequestStatus(uint256 _requestId) external view returns (bool fulfilled, uint256[] memory randomWords) {
+        require(s_requests[_requestId].exists, 'request not found');
+        RequestStatus memory request = s_requests[_requestId];
+        return (request.fulfilled, request.randomWords);
+    }
+
 }
