d701ac5d0f6f0be81284cd7bc5b70b3d4cc9153f
Amogh <amoghjahagirdargsf@gmail.com>
2022-10-29 16:15:05 +0400

    wip

--- src/Game.sol
+++ src/Game.sol
@@ -1,8 +1,20 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Game {
-    constructor(owner address){
+struct Coordinates {
+    int256 x; 
+    int256 y; 
+}
 
+contract Game is Ownable {
+    address owner;
+    uint256 roundNumber;
+    uint256 gameNumber;
+    Coordinates[] mines;
+    
+    constructor(address _owner){
+        owner = _owner;
     }
+
 }
